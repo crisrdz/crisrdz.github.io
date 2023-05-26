ac5de82f27de6a454d0b0b5795c09149849ae4d7
Cristofer Rodríguez <cristofer.rodriguez.elgueta@gmail.com>
2023-05-26 14:19:21 -0400

    Se modifica texto de competencias

--- src/components/sections/Competencias.jsx
+++ src/components/sections/Competencias.jsx
@@ -8,7 +8,7 @@ function Competencias() {
           <h3 className="title">Competencias técnicas</h3>
           <div className="grid-section">
             <p>
-            Tengo conocimientos de Front-End básico (HTML, CSS, JavaScript), aparte de poseer conocimiento básico de Git y Github. Además, actualmente estoy interesado en ser un desarrollador fullstack del stack MERN (MongoDB, Express.js, React.js, Node.js), siendo eso en lo que me estoy centrando a día de hoy. Sin embargo, también he trabajado con el framework Laravel (en una experiencia laboral pasada) y el framework Django (este último de manera autodidacta, al igual que el stack MERN).
+            Tengo conocimientos de Front-End básico (HTML, CSS, JavaScript), aparte de poseer conocimiento básico de Git y Github. Además, actualmente estoy interesado en ser un desarrollador fullstack del stack MERN (MongoDB, Express.js, React.js, Node.js), siendo eso en lo que me estoy centrando a día de hoy. Sin embargo, también he trabajado con el framework Laravel en una experiencia laboral pasada.
             </p>
             <div className="icon-planet">
               <IoIosCodeWorking />
